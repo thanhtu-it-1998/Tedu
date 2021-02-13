154836bfc3ede35be8d6cf0b7d4db2df3de412b7
THANH TU TRUONG <truongthanhtu.it.1998@gmail.com>
2021-02-13 14:45:13 +0700

    Create CRUD with ajax

---
+++ JQueryAjaxCRUDInASPNetCore/JQueryAjaxCRUDInASPNetCore/Migrations/20210213064700_Dbv3.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JQueryAjaxCRUDInASPNetCore.Migrations
+{
+    public partial class Dbv3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SWIFTCode",
+                table: "TransactionModel",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BeneficiaryName",
+                table: "TransactionModel",
+                type: "nvarchar(100)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankName",
+                table: "TransactionModel",
+                type: "nvarchar(100)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountNumber",
+                table: "TransactionModel",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SWIFTCode",
+                table: "TransactionModel",
+                type: "nvarchar(11)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BeneficiaryName",
+                table: "TransactionModel",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankName",
+                table: "TransactionModel",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountNumber",
+                table: "TransactionModel",
+                type: "nvarchar(12)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+        }
+    }
+}
