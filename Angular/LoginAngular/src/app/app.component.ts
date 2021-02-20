4dd7d45d3fed974fd72ab85a48dabd616fabc75c
THANH TU TRUONG <truongthanhtu.it.1998@gmail.com>
2021-02-20 21:26:50 +0700

    Angular Parameterized Pipes

--- Angular/LoginAngular/src/app/app.component.ts
+++ Angular/LoginAngular/src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
-  })
-  export class AppComponent {
-    isValid: boolean = false;
-    ChangeData(valid:boolean){
-      this.isValid =valid;
-    }
-  }
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  students: any[] = [
+      {
+          ID: 'std101', Name: 'RaKesh ROUT',
+          DOB: '12/8/1988', Gender: 'Male', CourseFee: 1234.56
+      },
+      {
+          ID: 'std102', Name: 'ANURAG Mohanty', 
+          DOB: '10/14/1989', Gender: 'Male', CourseFee: 6666.00
+      },
+      {
+          ID: 'std103', Name: 'Priyanka Dewangan', 
+          DOB: '7/24/1992', Gender: 'Female', CourseFee: 6543.15
+      },
+      {
+          ID: 'std104', Name: 'Hina SHARMA', 
+          DOB: '8/19/1990', Gender: 'Female', CourseFee: 9000.50
+      },
+      {
+          ID: 'std105', Name: 'SamBIt SataPATHY',
+          DOB: '4/12/1991', Gender: 'Male', CourseFee: 9876.54
+      }
+  ];
+}
