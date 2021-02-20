9ffd2616895dd2a757ac0fcb9623654a7690b5e3
THANH TU TRUONG <truongthanhtu.it.1998@gmail.com>
2021-02-20 22:32:00 +0700

    Angular Custom Pipe

--- Angular/LoginAngular/src/app/app.module.ts
+++ Angular/LoginAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { MyTitlePipe } from './my-title.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyTitlePipe
   ],
 
   imports: [
