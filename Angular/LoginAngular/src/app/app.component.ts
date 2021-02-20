8210c2e854ec4e540e189a7d50956f9d5473a5d3
THANH TU TRUONG <truongthanhtu.it.1998@gmail.com>
2021-02-20 20:22:05 +0700

    Angular ngFor trackBy

--- Angular/LoginAngular/src/app/app.component.ts
+++ Angular/LoginAngular/src/app/app.component.ts
@@ -6,26 +6,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  students: any[] = [
-    {
-        ID: 'std101', FirstName: 'Preety', LastName: 'Tiwary',
-        Branch: 'CSE', DOB: '29/02/1988', Gender: 'Female'
-    },
-    {
-        ID: 'std102', FirstName: 'Anurag', LastName: 'Mohanty', 
-        Branch: 'ETC', DOB: '23/05/1989', Gender: 'Male'
-    },
-    {
-        ID: 'std103', FirstName: 'Priyanka', LastName: 'Dewangan', 
-        Branch: 'CSE', DOB: '24/07/1992', Gender: 'Female'
-    },
-    {
-        ID: 'std104', FirstName: 'Hina', LastName: 'Sharma', 
-        Branch: 'ETC', DOB: '19/08/1990', Gender: 'Female'
-    },
-    {
-        ID: 'std105', FirstName: 'Sambit', LastName: 'Satapathy', 
-        Branch: 'CSE', DOB: '12/94/1991', Gender: 'Male'
+  students: any[];
+
+    constructor() {
+        this.students = [
+            {
+                ID: 'std101', FirstName: 'Santosh', LastName: 'Jena', Branch: 'CSE',
+                DOB: '29/02/1988', Gender: 'Male'
+            },
+            {
+                ID: 'std102', FirstName: 'Anurag', LastName: 'Mohanty', Branch: 'ETC',
+                DOB: '23/05/1989', Gender: 'Male'
+            },
+            {
+                ID: 'std103', FirstName: 'Priyanka', LastName: 'Dewangan', Branch: 'CSE',
+                DOB: '24/07/1992', Gender: 'Female'
+            },
+        ];
     }
-  ];
+
+    getStudents(): void {
+        this.students = [
+            {
+                ID: 'std101', FirstName: 'Santosh', LastName: 'Jena', Branch: 'CSE',
+                DOB: '29/02/1988', Gender: 'Male'
+            },
+            {
+                ID: 'std102', FirstName: 'Anurag', LastName: 'Mohanty', Branch: 'ETC',
+                DOB: '23/05/1989', Gender: 'Male'
+            },
+            {
+                ID: 'std103', FirstName: 'Priyanka', LastName: 'Dewangan', Branch: 'CSE',
+                DOB: '24/07/1992', Gender: 'Female'
+            },
+            {
+                ID: 'std104', FirstName: 'Hina', LastName: 'Sharma', Branch: 'ETC',
+                DOB: '19/08/1990', Gender: 'Female'
+            },
+            {
+                ID: 'std105', FirstName: 'Sambit', LastName: 'Satapathy', Branch: 'CSE',
+                DOB: '12/94/1991', Gender: 'Male'
+            }
+        ];
+    } 
 }
