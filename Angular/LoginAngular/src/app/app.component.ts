47c5b58149f1217e89af3d81195dda19757f1c4a
THANH TU TRUONG <truongthanhtu.it.1998@gmail.com>
2021-02-20 23:37:42 +0700

    Dependency Injection in Angular

--- Angular/LoginAngular/src/app/app.component.ts
+++ Angular/LoginAngular/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component} from '@angular/core';
-import {Router} from '@angular/router';
+import {StudentService} from './student.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[StudentService]
 })
-export class AppComponent {
+
+export class AppComponent {  
+    students:any[] = [];
+    pageTitle:string ='';
+    private _studentService: StudentService;
+
+    constructor(studentService: StudentService) {
+        this._studentService = studentService;
+     }
+
+    ngOnInit() {
+        this.students = this._studentService.getStudents();
+        this.pageTitle = this._studentService.getTitle();
+    }
 }
